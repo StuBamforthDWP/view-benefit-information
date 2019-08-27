606d9ffbbecba2e9b73ddd5428c646e05b8e58e1
Stu Bamforth <stuart.bamforth@engineering.digital.dwp.gov.uk>
2019-08-27 15:41:40 +0100

    v4 first commit - added search choice branch.

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,24 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/v4/search-choice', (req, res, next) => {
+    if(req.session.data.data == 'clear') {
+        req.session.data['first-name'] = ''
+        req.session.data['last-name'] = ''
+        req.session.data.postcode = ''
+        req.session.data['ni-number'] = ''
+        req.session.data['dob-day'] = ''
+        req.session.data['dob-month'] = ''
+        req.session.data['dob-year'] = ''
+        req.session.data.organisation = ''
+        req.session.data.data = ''
+        req.session.data['signed-in'] = 'yes'
+
+    }
+    res.locals = req.session.data
+    next();
+})
+
 
 router.get('/v3/citizen-details', (req, res, next) => {
     if(req.session.data.data == 'clear') {
